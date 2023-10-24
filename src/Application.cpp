760547da37fa475b8d6c7aa69ab138f783887f23
Maciej Kowalski <kowalski.exe@gmail.com>
2023-10-24 17:16:48 +0200

    triagle

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 int main(void) {
@@ -16,17 +18,28 @@ int main(void) {
 
   /* Make the window's context current */
   glfwMakeContextCurrent(window);
+  if (glewInit() != GLEW_OK) {
+    std::cout << "Glew error!" << std::endl;
+    return -1;
+  }
 
+  std::cout << glGetString(GL_VERSION) << std::endl;
+
+  float positions[] = {
+    -0.5f, -0.5f,
+     0.0f,  0.5f,
+     0.5f, -0.5f
+  };
+  unsigned int buffer;
+  glGenBuffers(1, &buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, buffer);
+  glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
   /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window))
   {
     /* Render here */
     glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_TRIANGLES);
-    glVertex2f(-0.5f, -0.5f);
-    glVertex2f(0.0f, 0.5f);
-    glVertex2f(0.5f, -0.5f);
-    glEnd();
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     /* Swap front and back buffers */
     glfwSwapBuffers(window);
